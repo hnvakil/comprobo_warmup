de96f47bc159014452c72ce5ff3fabf4a247a94e
Han V <hvakil@olin.edu>
2022-09-12 18:27:37 -0400

    Got square code working

--- warmup/warmup/square.py
+++ warmup/warmup/square.py
@@ -12,32 +12,33 @@ class SquareNode(Node):
 
     def __init__(self):
         super().__init__('square')
-        self.start_time = time.perf_counter
+        self.start_time = int(time.perf_counter())
         self.create_timer(0.1, self.run_loop)
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
         
 
     def rotate_neato(self, msg):
-        msg.linear.x = 0
-        msg.linear.y = 0
-        msg.angular.z = 0.5
+        msg.linear.x = 0.0
+        msg.angular.z = 3.14159/4
         self.vel_pub.publish(msg)
         time.sleep(2)
 
 
     def run_loop(self):
-        print(self.start_time)
+        self.current_time = int(time.perf_counter())
+        print("current time: " + str(self.current_time))
+        print("start time: " + str(self.start_time))
         msg = Twist()
 
-        if((self.start_time - self.current_time) < 1000):
-            msg.linear.y = 0.5
-            msg.angular.z = 0
+        if((self.current_time - self.start_time) < 2):
+            msg.linear.x = 0.25
+            msg.angular.z = 0.0
         else:
             self.rotate_neato(msg)
-            start_time = time.perf_counter
+            self.start_time = int(time.perf_counter())
 
         self.vel_pub.publish(msg)
-        current_time = time.perf_counter
+        
 
         
 
