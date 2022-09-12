b336095d79a4e752975821a45619fd0bbc21b7d8
Ally Bell <36202358+allybbell@users.noreply.github.com>
2022-09-12 17:25:02 -0400

    driving neato in square initial code

--- warmup/warmup/square.py
+++ warmup/warmup/square.py
@@ -1,21 +1,40 @@
-import rclpy
+import time,rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
 #import rospy
+float start_time, current_time
 
 class SquareNode(Node):
     def __init__(self):
         super().__init__('square')
         self.create_timer(0.1, self.run_loop)
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        start_time = time.perf_counter
         
+
+    def rotate_neato(self):
+        msg.linear.x = 0
+        msg.linear.y = 0
+        msg.angular.z = 0.5
+        self.vel_pub.publish(msg)
+        time.sleep(2)
+
 
     def run_loop(self):
         msg = Twist()
-        msg.linear.x = 0.5
-        msg.angular.z = 0.5
+
+        if((start_time - current_time) < 1000):
+            msg.linear.y = 0.5
+            msg.angular.z = 0
+        else:
+            rotate_neato()
+            start_time = time.perf_counter
+
         self.vel_pub.publish(msg)
+        current_time = time.perf_counter
+
+        
 
 def main(args=None):
     rclpy.init(args=args)
