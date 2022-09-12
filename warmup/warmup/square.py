ecacda532c68e4676b06feec815be7124ba6a0b0
Han V <hvakil@olin.edu>
2022-09-12 17:53:29 -0400

    Working on variable placement

--- warmup/warmup/square.py
+++ warmup/warmup/square.py
@@ -3,17 +3,21 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
 #import rospy
-float start_time, current_time
 
-class SquareNode(Node):
+
+class SquareNode(Node):    
+    
+    start_time = float(0)
+    current_time = float(0)
+
     def __init__(self):
         super().__init__('square')
+        self.start_time = time.perf_counter
         self.create_timer(0.1, self.run_loop)
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        start_time = time.perf_counter
         
 
-    def rotate_neato(self):
+    def rotate_neato(self, msg):
         msg.linear.x = 0
         msg.linear.y = 0
         msg.angular.z = 0.5
@@ -22,13 +26,14 @@ class SquareNode(Node):
 
 
     def run_loop(self):
+        print(self.start_time)
         msg = Twist()
 
-        if((start_time - current_time) < 1000):
+        if((self.start_time - self.current_time) < 1000):
             msg.linear.y = 0.5
             msg.angular.z = 0
         else:
-            rotate_neato()
+            self.rotate_neato(msg)
             start_time = time.perf_counter
 
         self.vel_pub.publish(msg)
@@ -37,6 +42,9 @@ class SquareNode(Node):
         
 
 def main(args=None):
+    global start_time 
+    global current_time
+
     rclpy.init(args=args)
     node = SquareNode()
     rclpy.spin(node)
