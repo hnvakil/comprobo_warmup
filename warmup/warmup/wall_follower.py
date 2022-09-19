febd5ddf8d3e5fea5bed370ae5b3ccf5b5da7f05
Han V <hvakil@olin.edu>
2022-09-19 18:48:14 -0400

    Made bag files for test bag and all behaviors

--- warmup/warmup/wall_follower.py
+++ warmup/warmup/wall_follower.py
@@ -1,6 +1,5 @@
 import rclpy
 import time
-import numpy as np
 import math
 import tty
 import select
@@ -19,38 +18,14 @@ class WallFollowerNode(Node):
         self.scan = [0]
 
 
-    def get_nearest_point_angle(self, center_angle, angle_range):
-        '''
-        angle range is to either side
-        '''
-        if self.scan is None:
-            return 0, self.goal_dist
-        #print(self.scan)
-        doubled_scan = np.concatenate([self.scan, self.scan])
-        for reading in range(len(doubled_scan)):
-            if doubled_scan[reading] == 0.0:
-                doubled_scan[reading] = 200
-        start_index = center_angle + 360 - angle_range
-        end_index = center_angle + 360 + angle_range
-        scan_range = doubled_scan[start_index:end_index+1]
-        try:
-            nearest_dist = min(scan_range)
-        except:
-            return 0, 200 + 1
-
-        nearest_point_angle = self.scan.index(nearest_dist)
-        return nearest_point_angle, nearest_dist
-    
-    def get_nearest_point(self):
-        return self.get_nearest_point_angle(0, 180)
-    
-    
     def run_loop(self):
         msg = Twist()
         # run scan
 
         # find min dist and heading
-        min_dist, min_dist_heading = self.get_nearest_point()
+        print(self.scan)
+        min_dist = min(self.scan)
+        min_dist_heading = self.scan.index(min_dist)
         print("Min dist: " + str(min_dist) + " of type: " + str(type(min_dist)))
         print("Min dist heading: " + str(min_dist_heading) + " of type: " + str(type(min_dist_heading)))
 
@@ -60,12 +35,12 @@ class WallFollowerNode(Node):
         if min_dist_heading > 90: #pos dir is ccw turn which turns left
             dir = 1
         
-        msg.angular.z = abs(min_dist_heading - 90) * dir / 180 /10
+        msg.angular.z = abs(min_dist_heading - 90) * dir / 180
         
 
 
         #if heading within margin of error drive forward
-        if abs(min_dist_heading - 90) <=5:
+        if abs(min_dist_heading - 90) <=1:
             print('going')
             msg.linear.x = 0.2
         self.vel_pub.publish(msg)
