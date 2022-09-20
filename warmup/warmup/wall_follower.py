2ca9497d5f34dd49c4183c8a8843d6c9d97382d4
Han V <hvakil@olin.edu>
2022-09-19 20:26:15 -0400

    Added marker to wall following and made new bag

--- warmup/warmup/wall_follower.py
+++ warmup/warmup/wall_follower.py
@@ -8,24 +8,45 @@ import termios
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from visualization_msgs.msg import Marker
 
 class WallFollowerNode(Node):
     def __init__(self):
         super().__init__('wall_follower')
         self.create_timer(0.1, self.run_loop)
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.marker_pub = self.create_publisher(Marker, 'marker', 10)
         self.create_subscription(LaserScan, 'stable_scan', self.process_scan, 10)
         self.scan = [0]
 
 
     def run_loop(self):
         msg = Twist()
+        marker = Marker()
+        marker.header.frame_id="base_link"
+        marker.ns = "basic_shapes"
+        marker.id = 0
+        marker.type = marker.SPHERE
+        marker.action = marker.ADD
         # run scan
 
         # find min dist and heading
         print(self.scan)
         min_dist = min(self.scan)
         min_dist_heading = self.scan.index(min_dist)
+
+
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.a = 1.0
+        marker.color.g = 1.0
+        marker.color.r = 0.5
+        marker.pose.position.z = 0.0
+        marker.pose.position.y = min_dist * math.sin(math.radians(min_dist_heading))
+        marker.pose.position.x = min_dist * math.cos(math.radians(min_dist_heading))
+
+
         print("Min dist: " + str(min_dist) + " of type: " + str(type(min_dist)))
         print("Min dist heading: " + str(min_dist_heading) + " of type: " + str(type(min_dist_heading)))
 
@@ -43,6 +64,7 @@ class WallFollowerNode(Node):
             print('going')
             msg.linear.x = 0.2
         self.vel_pub.publish(msg)
+        self.marker_pub.publish(marker)
         time.sleep(0.1)
         
 
